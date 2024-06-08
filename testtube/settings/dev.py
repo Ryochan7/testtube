21c48637abff893442e360f1d3cdb1ee2798966c
Travis Nickles <ryochan7@protonmail.com>
2024-06-08 17:21:06 -0500

    Change dev settings to have Django use nginx to host static and media files

--- testtube/settings/dev.py
+++ testtube/settings/dev.py
@@ -1,5 +1,13 @@
 from .common import *
 
-RESOURCE_PREFIX = ""
+### Using Django dev server to host media
+#RESOURCE_PREFIX = ""
+#STATIC_URL = "{}/static/".format(RESOURCE_PREFIX)
+#MEDIA_URL = "{}/media/".format(RESOURCE_PREFIX)
+
+### Using nginx to host media
+RESOURCE_PREFIX = "http://127.0.0.1:8080"
 STATIC_URL = "{}/static/".format(RESOURCE_PREFIX)
 MEDIA_URL = "{}/media/".format(RESOURCE_PREFIX)
+
+ENABLE_TOOLBAR = False
