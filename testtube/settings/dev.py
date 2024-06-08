a5c29f2f17af08bd44ddea298c4d7c1bccb72f02
Travis Nickles <ryochan7@protonmail.com>
2024-06-08 17:33:51 -0500

    Have repo file use Django dev server for hosting media

--- testtube/settings/dev.py
+++ testtube/settings/dev.py
@@ -1,13 +1,13 @@
 from .common import *
 
 ### Using Django dev server to host media
-#RESOURCE_PREFIX = ""
-#STATIC_URL = "{}/static/".format(RESOURCE_PREFIX)
-#MEDIA_URL = "{}/media/".format(RESOURCE_PREFIX)
-
-### Using nginx to host media
-RESOURCE_PREFIX = "http://127.0.0.1:8080"
+RESOURCE_PREFIX = ""
 STATIC_URL = "{}/static/".format(RESOURCE_PREFIX)
 MEDIA_URL = "{}/media/".format(RESOURCE_PREFIX)
+
+### Using nginx to host media
+#RESOURCE_PREFIX = "http://127.0.0.1:8080"
+#STATIC_URL = "{}/static/".format(RESOURCE_PREFIX)
+#MEDIA_URL = "{}/media/".format(RESOURCE_PREFIX)
 
 ENABLE_TOOLBAR = False
